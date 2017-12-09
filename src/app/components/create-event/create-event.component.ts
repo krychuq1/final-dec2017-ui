5bb56875bec31d685f2ca792eb82f8f11f107ee0
Unknown <krys.nagorny@gmail.com>
2017-12-09 19:11:40 +0100

    event

--- src/app/components/create-event/create-event.component.ts
+++ src/app/components/create-event/create-event.component.ts
@@ -2,6 +2,8 @@ import {Component, OnInit} from '@angular/core';
 import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 import {EventService} from '../../services/event.service';
 import {UserService} from '../../services/user.service';
+import {AppComponent} from '../../app.component';
+import {Router} from "@angular/router";
 
 @Component({
     selector: 'event-create',
@@ -19,11 +21,18 @@ export class CreateEventComponent implements OnInit {
   endDateController;
   cityController;
   NUMBER_PATTERN = /^[0-9]*$/;
+  TIME_PATTERN = /^([0-9]|0[0-9]|1[0-9]|2[0-3]):[0-5][0-9]$/;
+  startTimeController;
+  endTimeController;
+  categoryController;
   img;
+  imgError;
+  img_url;
 
   ngOnInit(): void {
   }
-  constructor(private formBuilder: FormBuilder, private eventService: EventService, private userService: UserService) {
+  constructor(private formBuilder: FormBuilder, private eventService: EventService, private userService: UserService,
+              private appComponent: AppComponent, private router: Router) {
     this.buildForm();
   }
   private buildForm() {
@@ -35,7 +44,10 @@ export class CreateEventComponent implements OnInit {
       city: this.formBuilder.control(null, [Validators.required, Validators.minLength(2)]),
       numberOfPlaces: this.formBuilder.control(null, [Validators.required, Validators.pattern(this.NUMBER_PATTERN)]),
       startDate: this.formBuilder.control(null, [Validators.required]),
-      endDate: this.formBuilder.control(null, [Validators.required])
+      endDate: this.formBuilder.control(null, [Validators.required]),
+      startTime: this.formBuilder.control(null, [Validators.required, Validators.pattern(this.TIME_PATTERN)]),
+      category: this.formBuilder.control(null, [Validators.required, Validators.minLength(2)]),
+      endTime: this.formBuilder.control(null, [Validators.required,  Validators.pattern(this.TIME_PATTERN)])
 
     });
     this.titleController = this.eventForm.get('title');
@@ -46,37 +58,56 @@ export class CreateEventComponent implements OnInit {
     this.startDateController = this.eventForm.get('startDate');
     this.endDateController = this.eventForm.get('endDate');
     this.cityController = this.eventForm.get('city');
+    this.startTimeController = this.eventForm.get('startTime');
+    this.endTimeController = this.eventForm.get('endTime');
+    this.categoryController = this.eventForm.get('category');
 
   }
   public onUploadFinished(event) {
     console.log('img updated', event);
     this.img = event;
-    // this.eventService.saveImage(event).subscribe(res => {
-    //   console.log(res);
-    // }, err => {
-    //   console.log(err);
-    // });
-  }
-  public createEvent() {
-    const event = {
-      title: this.titleController.value,
-      address: this.locationController.value,
-      online_event: false,
-      start_date: this.startDateController.value.toString(),
-      end_date: this.endDateController.value.toString(),
-      description: this.descriptionController.value,
-      number_of_places: this.numberOfPlacesController.value,
-      organizer_name: this.organizerController.value,
-      city: this.cityController.value
-    };
-    const  token = this.userService.getLocalUser().token;
-    this.eventService.createEvent(token, event).subscribe(res => {
-      console.log(res);
+    this.imgError = false;
+    this.eventService.saveImage(this.img).subscribe(res => {
+      this.img_url = res['imgUrl'];
+      console.log(this.img_url );
     }, err => {
+      this.imgError = 'Image is too big please upload img smaller than 5mb';
       console.log(err);
     });
   }
+  public createEvent() {
+    if (this.img) {
+      const event = {
+        title: this.titleController.value,
+        address: this.locationController.value,
+        online_event: false,
+        start_date: this.startDateController.value.toString(),
+        end_date: this.endDateController.value.toString(),
+        description: this.descriptionController.value,
+        number_of_places: this.numberOfPlacesController.value,
+        organizer_name: this.organizerController.value,
+        city: this.cityController.value,
+        start_time: this.startTimeController.value,
+        end_time: this.endTimeController.value,
+        category: this.categoryController.value,
+        image: this.img_url
+
+      };
+      const  token = this.userService.getLocalUser().token;
+      this.eventService.createEvent(token, event).subscribe(res => {
+        console.log(res);
+        this.appComponent.checkLocalStorage();
+        this.router.navigateByUrl('');
+      }, err => {
+        console.log(err);
+      });
+    }else {
+        this.imgError = 'Please upload image';
+    }
+
+  }
   public onRemoved() {
     this.img = null;
+    this.imgError = ''
   }
 }
